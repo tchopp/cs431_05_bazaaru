41d4a41ee0187d3fcfbffd5c5b86647158d081a4
Christopher Chopping <ctc111@rutgers.edu>
2022-10-04 20:09:43 -0400

    updated example test for node

--- typescript-nodejs/index.ts
+++ typescript-nodejs/index.ts
@@ -3,7 +3,12 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
+    res.attachment('/Bubble_Tea.png');
     res.send('Hello BazaaRu devs! <3');
+})
+
+app.get('/testing', (req, res) => {
+    res.send('blah');
 })
 
 app.listen(3000, () => {
