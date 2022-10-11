9447ef9b3ac140b0a480ca04ed6834e863a624f1
WhlteSnow <chaeandy21@gmail.com>
2022-10-11 00:37:45 -0400

    Basic Website Outline Framework Completion

--- BazaaRU/FrontEndServer/src/Pages/Home.js
+++ BazaaRU/FrontEndServer/src/Pages/Home.js
@@ -1,17 +1,47 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import logo from "./profile.png"; //Need to replace image with project image
 import "./Home.css";
 
 //import { useHistory } from "react-router-dom";
 
 function Home() {
+  const styles = {
+    fontSize: 20,
+    marginRight: "20px",
+  };
+  const navigate = useNavigate();
+  function signOut() {
+    //console.log("pressed");
+    navigate("/");
+  }
+  function toCatalog() {
+    //console.log("pressed");
+    navigate("/homepage/catalog");
+  }
+  function toAbout() {
+    //console.log("pressed");
+    navigate("/homepage/about");
+  }
   return (
     <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Rutgers BazaaRU</p>
       </header>
-      <p>-Insert Homepage Design here</p>
+      <header className="Service-bar">
+        <button style={styles} onClick={toAbout}>
+          About
+        </button>
+        <button style={styles} onClick={toCatalog}>
+          Product Catalog
+        </button>
+        <button style={styles} onClick={signOut}>
+          Sign Out
+        </button>
+      </header>
+      <p>-Insert Basic Catalog Here</p>
     </div>
   );
 }
