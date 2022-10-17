4367cd622d4b718142714b8a22670e19c4fe8225
Yousof A <yousofakbarzai@gmail.com>
2022-10-16 20:28:19 -0400

    Updating Catalog.js
    Adding the Catalog component to the Catalog Page

--- BazaaRU/FrontEndServer/src/Pages/Catalog.js
+++ BazaaRU/FrontEndServer/src/Pages/Catalog.js
@@ -3,15 +3,15 @@ import logo from "./profile.png"; //Need to replace image with project image
 
 //import { useHistory } from "react-router-dom";
 
-function Catalog() {
+function CatalogPage() {
   return (
     <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>-insert catalog listing here</p>
+        <Catalog />
       </header>
     </div>
   );
 }
 
-export default Catalog;
+export default CatalogPage;
