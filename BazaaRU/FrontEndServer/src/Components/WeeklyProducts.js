8acf35204858c47b17cd3f0b87cdf046d52aa498
Yousof Akbarzai <yousofakbarzai@gmail.com>
2022-10-31 18:01:29 -0400

    Async DB call for Products

--- BazaaRU/FrontEndServer/src/Components/WeeklyProducts.js
+++ BazaaRU/FrontEndServer/src/Components/WeeklyProducts.js
@@ -1,17 +1,17 @@
 import React, {useEffect, useState} from 'react';
-import  {Post} from '../Post/Post.js';
+import  {Post} from './Post/Post.js';
 import axios from "axios";
 
 
 
-export const Catalog = () => {
+export const CatalogWeekly = () => {
 
 // 10 (-1) post_ids for first render
-const [posts,setPosts] = useState([0,0,0,0,0]);
+const [posts,setPosts] = useState([0,0]);
 
 
 
-useEffect(()=>{axios.get('http://cs431-05.cs.rutgers.edu:5000/catalog').then((response) => {
+useEffect(()=>{axios.get('http://cs431-05.cs.rutgers.edu:5000/catalogweek').then((response) => {
       const ids = [];
       for (let i = 0; i < response.data[0].length; i++) {
          ids.push(response.data[0][i].post_id);
