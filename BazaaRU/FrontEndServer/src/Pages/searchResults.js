01ef171cb57aa520f6c622d02b459642eea89479
YAhtiW <102620528+YAhtiW@users.noreply.github.com>
2022-10-31 23:00:08 -0400

    Update searchResults.js

--- BazaaRU/FrontEndServer/src/Pages/searchResults.js
+++ BazaaRU/FrontEndServer/src/Pages/searchResults.js
@@ -1,18 +1,43 @@
-import React from "react";
-import logo from "./profile.png"; //Need to replace image with project image
+import React from 'react';
+import { Navigate } from "react-router-dom";
+import logo from "./profile.png";
+import { useNavigate } from "react-router-dom";
+import {SearchPost} from '../Components/Post/SearchPost.js'
 
-//import { useHistory } from "react-router-dom";
-import {Results} from '../Components/Results/searchResults.js'
 
-function resultsList() {
-  return (
-    <div>
-      <header  className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Results />
+const SearchResults = (props) => {
+
+  var { keyword } = document.forms[0]; //grabs inputted information
+
+  const navigate = useNavigate();
+  
+  const handleSubmit = (e) => {
+     e.preventDefault();
+     
+
+    //C2. Send the object to express server via axios
+
+    //C3. Direct to a different page
+    navigate("/homepage/results", { state: "gutter" });
+  };
+  return (<div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Rutgers BazaaRU</p>
+      <header className = "searchBar">
+        <div class="form-container">
+        <form class="form" onSubmit={handleSubmit}>
+        <input name="search"
+        type="input"
+        placeholder="search..."/>
+        <button>Submit</button>
+        </form>
+        </div>
       </header>
-    </div>
-  );
-}
-
-export default resultsList;
+      <SearchPost kw = {"gutter"}></SearchPost>
+      </header>
+  
+      </div>)
+  
+  };
+  export default SearchResults;
