5d19a53910f0f03e5f53b0b4b50f4e19afd98b22
YAhtiW <102620528+YAhtiW@users.noreply.github.com>
2022-12-05 22:30:15 -0500

    Rating Average Added to Account Search

--- BazaaRU/FrontEndServer/src/Components/Accounts/AC.js
+++ BazaaRU/FrontEndServer/src/Components/Accounts/AC.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./AC.css";
 import axios from "axios";
 import { Navigate, useNavigate } from 'react-router-dom';
+import Rating from './Components/Review/Rating';
 
 export const AC = (props) => {
   const [ACData, setACData] = useState({
@@ -22,13 +23,6 @@ export const AC = (props) => {
       });
   }, [props.ac_id]);
 
-  function getRating(username) {
-    // send in user name
-    // get the number of reviews about the person
-    // get what their current average is
-    //(((current average * number of reviews) + new rating )/ number of reviews +1)
-  }
-
 
 const toACDetails = () => {
     navigate('/homepage/publicprofile/' + ACData.username);
@@ -36,7 +30,7 @@ const toACDetails = () => {
 
 return (<div className="Account"><ul onClick={toACDetails}>
             <li>{ACData.username}</li>
-            <li>{ACData.rating}</li>
+            <Rating reviewee = {ACData.username}></Rating>
             <li>-------------------------------</li>
         </ul>
         </div>);
