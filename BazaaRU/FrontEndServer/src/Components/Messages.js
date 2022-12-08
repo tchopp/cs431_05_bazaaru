728da45d5d46bcb2e9e7d2be6c6bf21ed82d7773
Yousof Akbarzai <yousofakbarzai@gmail.com>
2022-12-07 20:04:45 -0500

    Removed NavBar from Messages

--- BazaaRU/FrontEndServer/src/Components/Messages.js
+++ BazaaRU/FrontEndServer/src/Components/Messages.js
@@ -1,10 +1,9 @@
-import { TopHalf } from "./TopHalf";
+
 import {Chat} from "./Chat";
 import { Fragment } from "react";
 const Messages = () => { 
     return (
         <Fragment>
-            <TopHalf />
             <Chat />
         </Fragment>
     );
