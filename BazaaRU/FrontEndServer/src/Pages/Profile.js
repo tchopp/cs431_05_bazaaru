6258255dcc39c59e69e0c57d15ade447939b375c
tchopp <82618391+tchopp@users.noreply.github.com>
2022-12-09 05:37:59 -0500

    Update Profile.js

--- BazaaRU/FrontEndServer/src/Pages/Profile.js
+++ BazaaRU/FrontEndServer/src/Pages/Profile.js
@@ -1,8 +1,12 @@
+import React from "react";
 import logo from "./profile.png";
 import "./Profile.css";
 import { useNavigate } from "react-router-dom";
 import Account from "../Components/Account";
 import PasswordChange from "../Components/PasswordChange";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
 
 function Profile() {
   const navigate = useNavigate();
@@ -10,6 +14,18 @@ function Profile() {
     fontSize: 20,
     marginRight: "20px",
   };
+
+  const [permID, setPermID] = useState("");
+  const username = Cookies.get("userName");
+  const reqData = { username: username };
+  useEffect(() => {
+    axios
+      .post("http://cs431-05.cs.rutgers.edu:5000/accountRank", reqData)
+      .then((response) => {
+        setPermID(response.data.permID);
+      });
+  });
+
   function toPurchaseHistory() {
     navigate("/homepage/profile/transactions");
   }
@@ -19,10 +35,17 @@ function Profile() {
   function toProfileTest() {
     navigate("/homepage/profile/profiletest");
   }
+  function toAccountList() {
+    navigate("/homepage/accountList");
+  }
+  function toContactList() {
+    navigate("/homepage/userContacts");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo"  />
         <p>Account Profile</p>
       </header>
       <Account></Account>
@@ -35,6 +58,16 @@ function Profile() {
       <button style={styles} onClick={toProfileTest}>
         Search Profile
       </button>
+      {(permID === 3 || permID === 2) && (
+        <button style={styles} onClick={toAccountList}>
+          Account List
+        </button>
+        )}
+      {(permID === 3 || permID === 2) && (
+        <button style={styles} onClick={toContactList}>
+          User Feedback
+        </button>
+      )}
       <PasswordChange></PasswordChange>
     </div>
   );
