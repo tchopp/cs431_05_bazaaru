76e15ce935137526c18234c1b014cd1e39c98a3a
Yousof Akbarzai <yousofakbarzai@gmail.com>
2022-12-11 16:15:25 -0500

    ResizePhoto

--- BazaaRU/FrontEndServer/src/Pages/searchResults.js
+++ BazaaRU/FrontEndServer/src/Pages/searchResults.js
@@ -7,6 +7,7 @@ import {SearchPost} from '../Components/Post/SearchPost.js'
 import {SearchAC} from '../Components/Accounts/SearchAC.js'
 import SearchBar from '../Components/Search/SearchBar'
 import NavBar from '../Components/NavBar'
+import { Container } from '@mui/system';
 
 
 const SearchResults = (props) => {
@@ -26,7 +27,9 @@ const SearchResults = (props) => {
       <NavBar></NavBar>
       <SearchBar></SearchBar>
       <SearchAC ac_KW = {passkeyword}></SearchAC>
+      <Container maxWidth={'xs'} maxHeight={'xs'} spacing={30} gap={30}>
       <SearchPost post_KW = {passkeyword}></SearchPost>
+      </Container>
       </header>
   
       </div>
